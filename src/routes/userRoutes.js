8db363925d845c9c1867f9f45b9d5e51451488f6
Narsimmayemul <narsimmayemul49450@gmail.com>
2024-06-19 13:34:50 +0530

    done

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// router.post('/signup', userController.signup);
-router.post('/login', userController.login);
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: APIs for managing users
+ */
 
 /**
  * @swagger
@@ -22,6 +26,9 @@ router.post('/login', userController.login);
  *         password:
  *           type: string
  *           description: The user's password
+ *         email:
+ *           type: string
+ *           description: The user's email address
  *       example:
  *         username: testuser
  *         password: password123
@@ -48,5 +55,34 @@ router.post('/login', userController.login);
  */
 router.post('/signup', userController.signup);
 
+/**
+ * @swagger
+ * /api/users/login:
+ *   post:
+ *     summary: Log in an existing user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: The user's username
+ *               password:
+ *                 type: string
+ *                 description: The user's password
+ *             example:
+ *               username: testuser
+ *               password: password123
+ *     responses:
+ *       200:
+ *         description: User successfully logged in
+ *       401:
+ *         description: Unauthorized, invalid credentials
+ */
+router.post('/login', userController.login);
 
 module.exports = router;
